5afe2629187b5e7ecad82876368931311e5aea7e
Raul Raat <raulraat@gmail.com>
2018-09-10 23:28:22 +0300

    add database logic for getFeedById and getFeedItems

--- api/controllers/feed.js
+++ api/controllers/feed.js
@@ -1,3 +1,6 @@
+var mysql = require('mysql');
+var config = require('../../config/config.js');
+
 module.exports = {
   addFeed: addFeed,
   getFeedById: getFeedById,
@@ -10,12 +13,45 @@ function addFeed(req, res) {
 }
 
 function getFeedById(req, res) {
-  const feedId = req.swagger.params.feedId.value || null;
-  // FIXME: Not Implemented
-  res.status(501).json();
+  var feedId = req.swagger.params.feedId.value || null;
+  var connection = mysql.createConnection(config.database);
+  connection.connect();
+  var sql = "SELECT * FROM feeds WHERE id = ?";
+  connection.query(sql, [feedId], function (error, results) {
+    if (error) {
+      console.log("getFeedById failed", error);
+      return;
+    }
+    if (results && results.length) {
+      res.json(results[0]);
+    } else {
+      res.status(404).json();
+    }
+  });
+  connection.end();
 }
 
 function getFeedItems(req, res) {
-  // FIXME: Not Implemented
-  res.status(501).json();
+  // FIXME: pagination
+  var feedId = req.swagger.params.feedId.value || null;
+  if (!feedId || feedId < 1) {
+    res.status(400).json();
+    return;
+  }
+  var connection = mysql.createConnection(config.database);
+  connection.connect();
+  var sql = "SELECT * FROM items WHERE feed_id = ?";
+  connection.query(sql, [feedId], function (error, results) {
+    if (error) {
+      console.log("getFeedItems failed", error);
+      res.status(500).json();
+      return;
+    }
+    if (results && results.length) {
+      res.json(results);
+    } else {
+      res.status(404).json();
+    }
+  });
+  connection.end();
 }
